40d4e253c244db27044f23619e220bd755723b5a
gdbranco <gdbranco@gmail.com>
2016-04-16 19:42:02 -0300

    script fix -criar diretorio de saida

--- script.py
+++ script.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 import subprocess
 import time
-#Reverse
+import os
 sort = ["Bubble","Comb","Insertion","Merge","Selection","Shell"]
 files = ["Reversed","Shuffle","Normal"]
 archives = 20
 times = 1
 for mode in sort:
+	outpath = "./Saidas/" + mode + "/"
+	if not os.path.exists(outpath):
+		print "Caminho: " + outpath + "\nNao existe e sera criado"
+		os.makedirs(outpath)
 	for type in files:
+		inpath = "./Entradas/" + type + "/"
 		num = 10
 		sum = float(0)
 		for i in range(0,archives):
-			infile_name = "./Entradas/" + type + "/entrada_" + str(num) + "_" + type.lower() + ".txt"
+			infile_name = inpath + "entrada_" + str(num) + "_" + type.lower() + ".txt"
 			cmd = "timeout 1800 ./" + mode.lower() + ".out <" + infile_name
 			print "Running: " + cmd
 			for j in range(0,times):
@@ -21,7 +26,7 @@ for mode in sort:
 				sum += float(process.stdout.read())
 				time.sleep(2)
 			sum/=times
-			outfile_name = "./Saidas/" + mode + "/saida_" + type.lower() + ".csv";
+			outfile_name = outpath + "saida_" + type.lower() + ".csv";
 			with open(outfile_name,"a") as f:
 				duracao = "{0:.6f}".format(sum)
 				print "Escrevendo: " + outfile_name 
